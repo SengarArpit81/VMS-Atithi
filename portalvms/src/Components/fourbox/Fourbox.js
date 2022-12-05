e330936c1d98da9abff82dd0ad099133add1b8eb
ArpitSengar <sengar.apt85@gmail.com>
2022-12-05 11:45:15 +0530

    Companydetailsupdated

--- portalvms/src/Components/fourbox/Fourbox.js
+++ portalvms/src/Components/fourbox/Fourbox.js
@@ -1,19 +1,23 @@
 import { Col, Row } from "antd";
 import React from "react";
+import { Nav } from "react-bootstrap";
 import Card from "react-bootstrap/Card";
+import { Link } from "react-router-dom";
+
+import CompanyDetails from "../CompanyDetails/CompanyDetails";
 import "./fourbox.css"
 const Fourbox = () => {
   return (
     <div>
       <Row>
         <Col span={24} >
-          <Row  className="Main-Card-Layout-Div">
-            <Col span={6} xs={24} sm={12} md={12} lg={12} xl={6}  className="d-flex justify-content-center align-items-center ">
+          <Row className="Main-Card-Layout-Div">
+            <Col span={6} xs={24} sm={12} md={12} lg={12} xl={6} className="d-flex justify-content-center align-items-center ">
               <div>
-                <Card  className="Main-Card-Div Animation-Card-Left" >
+                <Card className="Main-Card-Div Animation-Card-Left" >
                   {/* <Card.Header>Header</Card.Header> */}
                   <Card.Body className="d-flex justify-content-center align-items-center ">
-                    <Card.Title>Primary Card Title</Card.Title>
+                  <Link className="linkstyles"  to ="/companydetails"> <Card.Title>Add Visitors</Card.Title></Link>
                     {/* <Card.Text>
                       Some quick example text to build on the card title and
                       make up the bulk of the card's content.
@@ -27,7 +31,7 @@ const Fourbox = () => {
                 <Card className="Main-Card-Div Animation-Card-Top">
                   {/* <Card.Header>Header</Card.Header> */}
                   <Card.Body className="d-flex justify-content-center align-items-center ">
-                    <Card.Title>Primary Card Title</Card.Title>
+                  <Link className="linkstyles"  to ="/companydetails"> <Card.Title>Approved Visitor List</Card.Title></Link>
                     {/* <Card.Text>
                       Some quick example text to build on the card title and
                       make up the bulk of the card's content.
@@ -41,7 +45,7 @@ const Fourbox = () => {
                 <Card className="Main-Card-Div Animation-Card-Bottom">
                   {/* <Card.Header>Header</Card.Header> */}
                   <Card.Body className="d-flex justify-content-center align-items-center ">
-                    <Card.Title>Primary Card Title</Card.Title>
+                  <Link className="linkstyles"  to ="/companydetails"> <Card.Title>Rejected List Visitor</Card.Title></Link>
                     {/* <Card.Text>
                       Some quick example text to build on the card title and
                       make up the bulk of the card's content.
@@ -51,18 +55,28 @@ const Fourbox = () => {
               </div>
             </Col>
             <Col span={6} xs={24} sm={12} md={12} lg={12} xl={6} className="d-flex justify-content-center align-items-center ">
-              <div>
-                <Card className="Main-Card-Div Animation-Card-Right">
-                  {/* <Card.Header>Header</Card.Header> */}
-                  <Card.Body className="d-flex justify-content-center align-items-center ">
-                    <Card.Title>Primary Card Title</Card.Title>
-                    {/* <Card.Text>
+
+
+
+                <div>
+
+                  <Card className="Main-Card-Div Animation-Card-Right">
+                    {/* <Card.Header>Header</Card.Header> */}
+
+                    <Card.Body className="d-flex justify-content-center align-items-center ">
+                    <Link className="linkstyles"  to ="/companydetails">  <Card.Title>Company Details</Card.Title>              </Link>
+
+
+                      {/* <Card.Text>
                       Some quick example text to build on the card title and
                       make up the bulk of the card's content.
                     </Card.Text> */}
-                  </Card.Body>
-                </Card>
-              </div>
+                    </Card.Body>
+
+
+                  </Card>
+                </div>
+
             </Col>
           </Row>
         </Col>
